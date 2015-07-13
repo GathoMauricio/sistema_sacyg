684e920f888180143ea8665dc01c0788f7aba098
GathoMauricio <mauricio2769@gmail.com>
2015-07-12 19:00:15 -0500

    modales

---
+++ php/modal_reservacion.php
@@ -0,0 +1,36 @@
+<?php session_start() ?>
+<?php 
+include "../control/conexion.php";
+$consulta="SELECT * FROM sucursal";
+$datos=mysqli_query($conexion,$consulta);
+ ?>
+<!-- Modal -->
+<div class="modal fade" id="modal_reservacion" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel">Ingrese Los datos de su reservacion</h4>
+      </div>
+      <div class="modal-body">
+        <input type="text" id="id_cliente_reservacion" value="<?php echo $_SESSION['id_cliente']; ?>" hidden>
+        <label>Cliente</label>
+        <input type="text" class="form-control" value="<?php echo $_SESSION['nombre']; ?>" readonly>
+        <label>Sucursal</label>
+        <select class="form-control" onchange="cargaMesas(this.value);">
+          <?php 
+            while($fila=mysqli_fetch_array($datos))
+            {
+              echo '<option value="'.$fila['id_sucursal'].'">'.$fila['sucursal'].'</option>';
+            }
+           ?>
+        </select>
+      </div>
+      <div class="modal-footer">
+        <center>
+        <button type="button" class="btn btn-default" data-dismiss="modal">Terminar</button>
+        </center>
+      </div>
+  </div>
+</div>
+</div>
